f5cbc85362d57d41ab6b6945c4fc769b0c0cc04a
胡宜航 <huyihang.2022@qq.com>
2025-10-25 00:19:36 +0800

    feat:以及分类路由配置

--- views/Layout/components/LayoutHeader.vue
+++ views/Layout/components/LayoutHeader.vue
@@ -14,8 +14,12 @@ const categoryStore = useCategoryStore();
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+        <li
+          class="home"
+          v-for="item in categoryStore.categoryList"
+          :key="item.id"
+        >
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
