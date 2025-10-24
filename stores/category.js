a5d8c1c8860c26261ec53bff926e53e5edf68a03
胡宜航 <huyihang.2022@qq.com>
2025-10-24 13:11:01 +0800

    feat:day2 over

---
+++ stores/category.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+import { getCategoryAPI } from "@/api/layout";
+import { ref } from "vue";
+
+export const useCategoryStore = defineStore("category", () => {
+  //导航列表数据管理
+  //state 导航列表数据
+  const categoryList = ref([]);
+  // action 获取导航数据的方法
+  const getCateList = async () => {
+    const res = await getCategoryAPI();
+    console.log(res);
+    categoryList.value = res.result;
+  };
+
+  return { categoryList, getCateList };
+});
---
+++ views/Home/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+</script>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+  <!-- 测试组件的复用 -->
+   <home-panel title="新鲜好物" sub-title="新鲜好物,好多商品">
+    <div>
+      新鲜好物插槽
+    </div>
+   </home-panel>
+
+    <home-panel title="人气推荐" sub-title="人气推荐,好多商品">
+    <div>
+      人气推荐插槽
+    </div>
+   </home-panel>
+</template>
+
